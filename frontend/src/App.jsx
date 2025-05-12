e5f897788c151a60b1b9fd02540e5b591f088831
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2025-05-12 20:48:20 +0200

    Add new component 'MovieCard'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
 
   return (
-    <div></div>
+    <>
+      <MovieCard movie={{title: "Thunderbolts *", release_date: "2025"}}/>
+      <MovieCard movie={{title: "Fantastic Four: First Steps *", release_date: "2025"}}/>
+    </>
+
   )
 }
 
