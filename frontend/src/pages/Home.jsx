285f628fda0472255b011ee7c88538effe026372
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2025-05-13 16:06:41 +0200

    Added functionality to filter and render movies dynamically based on user input in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,48 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
-    const movies = [
-    {id: 1, title: "Thunderbolts *", release_date: "2025"},
-    {id: 2, title: "Fantastic Four: First Steps *", release_date: "2025"},
-    {id: 3, title: "Avengers: Secret Wars *", release_date: "2025"},
-    {id: 4, title: "Tron: Ares", release_date: "2025"},
-    {id: 5, title: "The Batman Part II", release_date: "2025"},
-    {id: 6, title: "Mission: Impossible – Dead Reckoning Part Two", release_date: "2025"},
-];
+  const [searchQuery, setSearchQuery] = useState("");
 
-const handleSearch = () => {
+  const movies = [
+    { id: 1, title: "Thunderbolts *", release_date: "2025" },
+    { id: 2, title: "Fantastic Four: First Steps *", release_date: "2025" },
+    { id: 3, title: "Avengers: Secret Wars *", release_date: "2025" },
+    { id: 4, title: "Tron: Ares", release_date: "2025" },
+    { id: 5, title: "The Batman Part II", release_date: "2025" },
+  ];
 
-}
+  const handleSearch = () => {
+    e.preventDefault();
+    setSearchQuery("");
+  };
 
-    return <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for movies..." className="search-input" />
-        </form>
-        <div className="movies-grid">
-            {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
-        </div>
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies
+          .filter((movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+          )
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
     </div>
+  );
 }
 
 export default Home;
