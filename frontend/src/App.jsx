e5c47afc1613af60392d045da68e5077858fb40c
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2025-05-13 12:02:01 +0200

    Cleared App.jsx again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
 
 function App() {
 
   return (
     <>
-      <MovieCard movie={{title: "Thunderbolts *", release_date: "2025"}}/>
-      <MovieCard movie={{title: "Fantastic Four: First Steps *", release_date: "2025"}}/>
     </>
-
   )
 }
 
