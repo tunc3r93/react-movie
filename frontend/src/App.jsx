ef249c42441978120d73c01d444ec8c71b12d912
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2025-05-13 22:34:14 +0200

    feat: add routing with react-router-dom for Home and Favorites pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+import Favorite from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <Home />
-    </>
-  )
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorite />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
