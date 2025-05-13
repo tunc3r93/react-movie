97c00c6076216e5c65babca0916f60572c952249
tunc3r93 <178416938+tunc3r93@users.noreply.github.com>
2025-05-13 12:10:16 +0200

    new movie objects for 2025 to the movies array in Home.jsx file

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    const movies = [
+    {id: 1, title: "Thunderbolts *", release_date: "2025"},
+    {id: 2, title: "Fantastic Four: First Steps *", release_date: "2025"},
+    {id: 3, title: "Avengers: Secret Wars *", release_date: "2025"},
+    {id: 4, title: "Tron: Ares", release_date: "2025"},
+    {id: 5, title: "The Batman Part II", release_date: "2025"},
+    {id: 6, title: "Mission: Impossible – Dead Reckoning Part Two", release_date: "2025"},
+];
+
+    return <div className="Home"></div>
+}
+
